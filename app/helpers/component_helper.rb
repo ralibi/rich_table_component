3bd268d744b3c2b698833362196dec156d10f882
ralibi <arymondralibi@gmail.com>
2013-07-26 09:29:27 +0700

    update row number for updated row

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -198,14 +198,19 @@ module ComponentHelper
   end
 
   def row_number local_var_assignments
-    counter = local_var_assignments["#{local_var_assignments[:rtc_partial].split('/').last}_counter".to_sym].presence || 0
-    offset = local_var_assignments[:offset]
     result = '<td class="numeric rtc_row_number">'
-    if offset
-      result << "#{counter + offset + 1}."
+    
+    if local_var_assignments[:rtc_partial].present?
+      counter = local_var_assignments["#{local_var_assignments[:rtc_partial].split('/').last}_counter".to_sym].presence || 0
+      offset = local_var_assignments[:offset]
+      if offset
+        result << "#{counter + offset + 1}."
+      else
+        result << '(baru)'
+      end
     else
-      result << '(baru)'
-    end
+        result << '(update)'
+    end  
     result << '</td>'
 
     result.html_safe
